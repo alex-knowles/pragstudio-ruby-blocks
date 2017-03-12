6b97d14012a2779f331031b7b3e589c7162ab172
Alex Knowles <alexk@datalogics.com>
2017-03-11 22:26:06 -0600

    Use 'reject' iterator.

--- scores.rb
+++ scores.rb
@@ -3,3 +3,7 @@ scores = [83, 71, 92, 64, 98, 87, 75, 69]
 high_scores = scores.select { |s| s > 80 }
 puts high_scores
 puts ""
+
+low_scores = scores.reject { |s| s > 80 }
+puts low_scores
+puts ""
