c7e7d01ca45f4341df4fedd88aba97cfbbacf319
Alex Knowles <alexk@datalogics.com>
2017-03-12 22:32:38 -0500

    Change ‘deal’ to accept a method parameter.
    ‘number_of_cards’ parameter is optional — specifies
    number of cards to yield to the block.

--- deal.rb
+++ deal.rb
@@ -1,18 +1,20 @@
-def deal
+def deal(number_of_cards = 1)
   if !block_given?
     puts "No deal!"
   else
-    faces = %w(Jack Queen King Ace)
-    suits = %w(Hearts Diamonds Spades Clubs)
-    random_face = faces.sample
-    random_suit = suits.sample
-    score = yield(random_face, random_suit)
-    puts "You scored a #{score}!"
+    number_of_cards.times do
+      faces = %w(Jack Queen King Ace)
+      suits = %w(Hearts Diamonds Spades Clubs)
+      random_face = faces.sample
+      random_suit = suits.sample
+      score = yield(random_face, random_suit)
+      puts "You scored a #{score}!"
+    end
   end
 end
 
 
-deal do |face, suit|
+deal(10) do |face, suit|
   puts "Dealt a #{face} of #{suit}"
   score = face.length + suit.length
   score
