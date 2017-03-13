e9c1b32ee9991aae968e165869f6fca8e22a0ce6
Alex Knowles <alexk@datalogics.com>
2017-03-12 22:28:02 -0500

    Define an ’n_times’ iterator.
    Accepts a parameter ’n’.

--- iterator.rb
+++ iterator.rb
@@ -7,3 +7,14 @@ three_times do |number|
   puts "#{number} pushup"
   puts "#{number} chinup"
 end
+puts ""
+
+def n_times(n)
+  1.upto(n) { |x| yield x }
+end
+
+n_times(5) do |n|
+  puts "#{n} situp"
+  puts "#{n} pushup"
+  puts "#{n} chinup"
+end
