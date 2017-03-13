4855e28c6cf6d97c8215d52001edf857374f2e4a
Alex Knowles <alexk@datalogics.com>
2017-03-12 22:21:25 -0500

    Check if a block was given or not.

--- deal.rb
+++ deal.rb
@@ -1,10 +1,14 @@
 def deal
-  faces = %w(Jack Queen King Ace)
-  suits = %w(Hearts Diamonds Spades Clubs)
-  random_face = faces.sample
-  random_suit = suits.sample
-  score = yield(random_face, random_suit)
-  puts "You scored a #{score}!"
+  if !block_given?
+    puts "No deal!"
+  else
+    faces = %w(Jack Queen King Ace)
+    suits = %w(Hearts Diamonds Spades Clubs)
+    random_face = faces.sample
+    random_suit = suits.sample
+    score = yield(random_face, random_suit)
+    puts "You scored a #{score}!"
+  end
 end
 
 
@@ -13,3 +17,7 @@ deal do |face, suit|
   score = face.length + suit.length
   score
 end
+puts ""
+
+deal
+puts ""
