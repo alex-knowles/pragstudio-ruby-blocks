f33b377fd57c6dd9c947f41f99d8d5c28719e09d
Alex Knowles <alexk@datalogics.com>
2017-03-13 22:45:38 -0500

    Define class method to manage database connection.

--- database_driver.rb
+++ database_driver.rb
@@ -22,11 +22,20 @@ class DatabaseDriver
     puts "Executing #{sql}..."
     # executes SQL
   end
+
+  def self.open(database, user, password)
+    driver = self.new(database, user, password)
+    return driver unless block_given?
+    driver.connect
+    begin
+      yield driver
+    ensure
+      driver.disconnect
+    end
+  end
 end
 
-driver = DatabaseDriver.new("my_database", "admin", "secret")
-
-driver.connect
-driver.execute("SELECT * FROM ORDERS")
-driver.execute("SELECT * FROM USERS")
-driver.disconnect
+DatabaseDriver.open("my_database", "admin", "secret") do |driver|
+  driver.execute("SELECT * FROM ORDERS")
+  driver.execute("SELECT * FROM USERS")
+end
