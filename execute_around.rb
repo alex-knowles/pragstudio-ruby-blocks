63cac131734053e381f44e2e5f16f28fa9e22215
Alex Knowles <alexk@datalogics.com>
2017-03-13 21:18:19 -0500

    Example of wrapping code with debugging.

--- execute_around.rb
+++ execute_around.rb
@@ -15,3 +15,16 @@ tag(:ul) do
   tag(:li) { "It shines!" }
   tag(:li) { "It mesmerizes!" }
 end
+puts ""
+
+
+def with_debugging
+  puts "Got Here!"
+  result = yield
+  puts "Result was #{result}"
+end
+
+with_debugging do
+  magic_number = (23 - Time.now.hour) * Math::PI
+end
+puts ""
