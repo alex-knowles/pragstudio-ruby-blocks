8efb8507730fe7b5332e44d70cff72fc0edaf77d
Alex Knowles <alexk@datalogics.com>
2017-04-29 14:07:37 -0500

    Factor out method make a string for a square.
    Express a square number as a sum.
    For instance 5 squared is:
    5 + 5 + 5 + 5 + 5

--- summation.rb
+++ summation.rb
@@ -2,17 +2,25 @@ def square(n)
   n * n
 end
 
+def format_square_as_sum(n)
+  result = ""
+  1.upto(n) do |i|
+    if (i == n)
+      result += n.to_s
+    else
+      result += "#{n.to_s} + "
+    end
+  end
+  result
+end
+
+
 summation = 0
 i_string = ""
 1.upto(10) do |i|
-  j_string = i.to_s
-  2.upto(i) do |j|
-    j_string += " + #{i.to_s}"
-  end
   i_squared = square(i)
   summation += i_squared
-  j_string += " = #{i_squared.to_s}"
-  puts j_string
+  puts "#{i_squared} = #{format_square_as_sum(i)}"
   if i > 1
     i_string += " + #{i_squared.to_s}"
   else
