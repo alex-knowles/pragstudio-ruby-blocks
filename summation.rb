28553e2d6fcb957ded51eac933f2b77c734cde8c
Alex Knowles <alexk@datalogics.com>
2017-04-29 22:41:30 -0500

    Define method for printing “sum of squares”.
    Also add a ‘main’ to the file.

--- summation.rb
+++ summation.rb
@@ -21,17 +21,20 @@ def summation_of_squares(range)
   array_to_square.reduce(0) { |sum, n| sum + n * n }
 end
 
+def print_sum_of_squares(range)
+  # print each square number and its component parts
+  range.each do |n|
+    puts "#{square(n)} = #{format_square_as_sum(n)}"
+  end
 
-range = 1..10
-
-# print each square number and its component parts
-range.each do |n|
-  puts "#{square(n)} = #{format_square_as_sum(n)}"
+  # calculate the summation of the squared numbers and
+  # print out the expression each square number being summed
+  squares = range.map { |n| square(n) }
+  summation_string = format_addends(squares)
+  puts
+  puts "#{summation_of_squares(range)} = #{summation_string}"
 end
 
-# calculate the summation of the squared numbers and
-# print out the expression each square number being summed
-squares = range.map { |n| square(n) }
-summation_string = format_addends(squares)
-puts
-puts "#{summation_of_squares(range)} = #{summation_string}"
+if __FILE__ == $0
+  print_sum_of_squares(1..10)
+end
